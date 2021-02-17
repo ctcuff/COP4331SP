b2647c39419255561a0b1a331401555c41ba10ed
Armintron <64503268+Armintron@users.noreply.github.com>
2021-02-17 12:27:41 -0500

    Interpret blank query as "show all" request

--- html/API/SearchContact.php
+++ html/API/SearchContact.php
@@ -17,8 +17,7 @@
 	else
 	{
       $conn->select_db("COP4331");
-      // everything will partially match with a blank field, so get rid of blank fields
-      $inData = array_filter($inData);
+
 
       // check is the User_ID represents a valid user
       $sql = $conn->prepare("SELECT * FROM USERS WHERE ID=?");
@@ -27,18 +26,30 @@
       $rows = $sql->get_result();
       if($rows->num_rows < 1) sendErrorMessage("BAD_ID");
 
-      // get the contacts from this user, given the information. note that we use a partial
-      // match here so that the user doesnt have to be exact.
-      // We have to use the CONCAT function because % isnt properly escaped in the prepare function
-      $sql = $conn->prepare("SELECT Contact_ID,First_Name,Last_Name,Phone,Email FROM CONTACTS WHERE
-         User_ID=? AND (
-         First_Name like CONCAT('%',?,'%') OR
-         Last_Name like CONCAT('%',?,'%') OR
-         Phone like CONCAT('%',?,'%') OR
-         Email like CONCAT('%',?,'%')
-         )
-         ");
-      $sql->bind_param("issss", $inData["User_ID"], $inData["Query"], $inData["Query"], $inData["Query"], $inData["Query"]);
+      // value used to represent a query to return all contacts the user has
+      $SHOW_ALL_QUERY = "";
+
+      if($inData["Query"] == $SHOW_ALL_QUERY)
+      {
+         $sql = $conn->prepare("SELECT Contact_ID,First_Name,Last_Name,Phone,Email FROM CONTACTS WHERE User_ID=?");
+         $sql->bind_param("i", $inData["User_ID"]);
+      }
+      else
+      {
+         // note that we use a partial match here
+         // so that the user doesnt have to be exact.
+         // We have to use the CONCAT function because % isnt properly escaped in the prepare function
+         $sql = $conn->prepare("SELECT Contact_ID,First_Name,Last_Name,Phone,Email FROM CONTACTS WHERE
+            User_ID=? AND (
+            First_Name like CONCAT('%',?,'%') OR
+            Last_Name like CONCAT('%',?,'%') OR
+            Phone like CONCAT('%',?,'%') OR
+            Email like CONCAT('%',?,'%')
+            )
+            ");
+         $sql->bind_param("issss", $inData["User_ID"], $inData["Query"], $inData["Query"], $inData["Query"], $inData["Query"]);
+      }
+
       $success = $sql->execute();
       // get result from query and get all result rows
       $rows = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
